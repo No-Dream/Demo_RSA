d6307f46fdebd32a77d299c144ccbadb9cc025fb
No-Dream <EleRD.YunShan@gmail.com>
2016-12-30 12:28:59 +0800

    Dec30: .net4.0 RSA Demo

---
+++ Demo_RSA/Demo_RSA/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo_RSA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        CreateRSA m_RSA;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_RSA = new CreateRSA();
+        }
+
+        private byte[] ReadFile(string p_Path)
+        {
+            if (!File.Exists(p_Path))
+            {
+                return null;
+            }
+
+            try
+            {
+                FileStream t_FS = File.OpenRead(p_Path);
+                BinaryReader t_Reader = new BinaryReader(t_FS);
+                byte[] t_RawBytes = new byte[t_FS.Length];
+                t_Reader.Read(t_RawBytes, 0, Convert.ToInt32(t_FS.Length));
+                t_Reader.Close();
+                t_FS.Close();
+
+                return t_RawBytes;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+
+        }
+
+        private bool WriteFile(string p_Path, byte[] p_FileBytes)
+        {
+            if (File.Exists(p_Path))
+            {
+                File.Delete(p_Path);
+            }
+
+            try
+            {
+                FileStream t_FS = File.OpenWrite(p_Path);
+                BinaryWriter t_Writer = new BinaryWriter(t_FS);
+                t_Writer.Write(p_FileBytes);
+                t_Writer.Close();
+                t_FS.Close();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+        }
+
+        private void EnFile_Click(object sender, EventArgs e)
+        {
+            string t_ReadPath = textBox1.Text;
+            string t_SavePath = textBox2.Text;
+
+            byte[] t_RawBytes = ReadFile(t_ReadPath);
+            byte[] t_SecBytes = RSA_API.API_EnFile(t_RawBytes, m_RSA.PublicKey, false);
+            bool t_Res = WriteFile(t_SavePath, t_SecBytes);
+        }
+
+        private void DeFile_Click(object sender, EventArgs e)
+        {
+            string t_ReadPath = textBox1.Text;
+            string t_SavePath = textBox2.Text;
+
+            byte[] t_SecBytes = ReadFile(t_ReadPath);
+            byte[] t_RawBytes = RSA_API.API_DeFile(t_SecBytes, m_RSA.PrivateKey, false);
+            bool t_Res = WriteFile(t_SavePath, t_RawBytes);
+        }
+
+        private void RSASign_Click(object sender, EventArgs e)
+        {
+            string t_ReadPath = textBox1.Text;
+            string t_SavePath = textBox2.Text;
+
+            byte[] t_RawBytes = ReadFile(t_ReadPath);
+            byte[] t_SecBytes = RSA_API.API_RSASign(t_RawBytes, m_RSA.PrivateKey);
+            bool t_Res = WriteFile(t_SavePath, t_SecBytes);
+        }
+
+        private void CheckSign_Click(object sender, EventArgs e)
+        {
+            string t_ReadPath = textBox1.Text;
+            string t_SavePath = textBox2.Text;
+
+            byte[] t_RawBytes = ReadFile(t_ReadPath);
+            byte[] t_SecBytes = ReadFile(t_SavePath);
+
+            bool t_Res = RSA_API.API_RSACheckSign(t_RawBytes, t_SecBytes, m_RSA.PublicKey);
+
+            MessageBox.Show(t_Res.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = open.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = save.FileName;
+            }
+        }
+    }
+}
---
+++ Demo_RSA/Demo_RSA/ReRSA.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Demo_RSA
+{
+    class CreateRSA
+    {
+        protected RSACryptoServiceProvider m_RSA;
+
+        string m_PublicKey;
+        string m_PrivateKey;
+
+        public string PublicKey
+        {
+            get { return m_PublicKey; }
+        }
+        public string PrivateKey
+        {
+            get { return m_PrivateKey; }
+        }
+
+        public CreateRSA()
+        {
+            m_RSA = new RSACryptoServiceProvider();
+
+            //Get Key
+            m_PublicKey = m_RSA.ToXmlString(false);
+            m_PrivateKey = m_RSA.ToXmlString(true);
+        }
+
+    }
+
+    static class RSA_API
+    {
+        public static byte[] API_EnFile(byte[] p_FileBytes, string p_PublicKey, bool p_Padding)
+        {
+            try
+            {
+                RSACryptoServiceProvider t_RSA = new RSACryptoServiceProvider();
+                t_RSA.FromXmlString(p_PublicKey);
+
+                //分段加密
+                int t_KeySize = t_RSA.KeySize / 8;
+                int t_BufferSize = t_KeySize - 11;
+
+                byte[] t_Buffer = new byte[t_BufferSize];
+                MemoryStream t_MS_In = new MemoryStream(p_FileBytes);
+                MemoryStream t_MS_Out = new MemoryStream();
+
+                while (true)
+                {
+                    int t_ReadLength = t_MS_In.Read(t_Buffer, 0, t_BufferSize);
+                    if (t_ReadLength <= 0)
+                    {
+                        break;
+                    }
+
+                    byte[] t_ReadBytes = new byte[t_ReadLength];
+                    Array.Copy(t_Buffer, 0, t_ReadBytes, 0, t_ReadLength);
+
+                    byte[] t_SecBytes = t_RSA.Encrypt(t_ReadBytes, p_Padding);
+                    t_MS_Out.Write(t_SecBytes, 0, t_SecBytes.Length);
+                }
+
+                return t_MS_Out.ToArray();
+            }
+            catch (CryptographicException e)
+            {
+                return null;
+            }
+        }
+
+        public static byte[] API_DeFile(byte[] p_FileBytes, string p_PrivateKey, bool p_Padding)
+        {
+            try
+            {
+                RSACryptoServiceProvider t_RSA = new RSACryptoServiceProvider();
+                t_RSA.FromXmlString(p_PrivateKey);
+
+                //分段解密
+                int t_KeySize = 128;
+
+                byte[] t_Buffer = new byte[t_KeySize];
+                MemoryStream t_MS_In = new MemoryStream(p_FileBytes);
+                MemoryStream t_MS_Out = new MemoryStream();
+
+                while (true)
+                {
+                    int t_ReadLine = t_MS_In.Read(t_Buffer, 0, t_KeySize);
+                    if (t_ReadLine <= 0)
+                    {
+                        break;
+                    }
+
+                    byte[] t_ReadBytes = new byte[t_ReadLine];
+                    Array.Copy(t_Buffer, 0, t_ReadBytes, 0, t_ReadLine);
+
+                    byte[] t_UnSecBytes = t_RSA.Decrypt(t_ReadBytes, p_Padding);
+                    t_MS_Out.Write(t_UnSecBytes, 0, t_UnSecBytes.Length);
+                }
+
+                return t_MS_Out.ToArray();
+            }
+            catch (CryptographicException e)
+            {
+                return null;
+            }
+        }
+
+        public static byte[] API_RSASign(byte[] p_RawDataBytes, string p_PrivateKey)
+        {
+            try
+            {
+                RSACryptoServiceProvider t_RSA = new RSACryptoServiceProvider();
+                t_RSA.FromXmlString(p_PrivateKey);
+
+                byte[] t_SignedData = t_RSA.SignData(p_RawDataBytes, new SHA1CryptoServiceProvider());
+                return t_SignedData;
+            }
+            catch (CryptographicException e)
+            {
+                return null;
+            }
+        }
+
+        public static bool API_RSACheckSign(byte[] p_RawDataBytes, byte[] p_SignedDataBytes, string m_PublicKey)
+        {
+            try
+            {
+                RSACryptoServiceProvider t_RSA = new RSACryptoServiceProvider();
+                t_RSA.FromXmlString(m_PublicKey);
+
+                return t_RSA.VerifyData(p_RawDataBytes, new SHA1CryptoServiceProvider(), p_SignedDataBytes);
+
+            }
+            catch (CryptographicException e)
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
